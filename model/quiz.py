6a216f0a912fcc871099af1b49618ddedd592ae7
PatrickHastings2 <pjhpersons@gmail.com>
2024-04-17 23:21:20 -0700

    catagories

--- model/quiz.py
+++ model/quiz.py
@@ -6,3 +6,12 @@ from sklearn.preprocessing import LabelEncoder
 
 # Load the data
 college_data = pd.read_csv('Final_Extended_Real_College_Dataset.csv')
+
+# Encode categorical data
+encoder = LabelEncoder()
+college_data['Location'] = encoder.fit_transform(college_data['Location'])
+college_data['Weather'] = encoder.fit_transform(college_data['Weather'])
+college_data['Public/Private'] = encoder.fit_transform(college_data['Public/Private'])
+college_data['Population Size'] = encoder.fit_transform(college_data['Population Size'])
+college_data['Tuition Preference'] = encoder.fit_transform(college_data['Tuition Preference'])
+college_data['Orientation'] = encoder.fit_transform(college_data['Orientation'])
